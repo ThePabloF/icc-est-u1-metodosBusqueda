06e1173ae076e668f23ff63080d494ec3ebb8312
Pablo Feijo <pfeijo34@gmail.com>
2025-04-15 07:28:52 -0500

    FEAT(UPS-001) Creación del proyecto

---
+++ src/main/java/ec/edu/ups/poo/App.java
@@ -0,0 +1,9 @@
+package ec.edu.ups.poo;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
